b70a04e0387f6b90eefefd0e4c9c2e6db45c2e2f
Luka Bracanovic <lbracano@gmail.com>
2023-01-23 21:43:34 +0100

    Create quiz page

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import { QuestionsProvider } from "./contexts/questions";
+
 import { Paths } from "./constants";
 import Landing from "./pages/landing";
+import Quiz from "./pages/quiz";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={Paths.Home} element={<Landing />} />
-        <Route path={Paths.Quiz} element={<div>Quiz</div>} />
-      </Routes>
-    </BrowserRouter>
+    <QuestionsProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path={Paths.Home} element={<Landing />} />
+          <Route path={Paths.Quiz} element={<Quiz />} />
+        </Routes>
+      </BrowserRouter>
+    </QuestionsProvider>
   );
 }
 
---
+++ src/pages/quiz.tsx
@@ -0,0 +1,58 @@
+import { Col, Container, Row, Spinner } from "react-bootstrap";
+import Questionnaire from "../components/questionnaire";
+import { useCallback, useEffect, useState } from "react";
+import { useQuestions } from "../contexts/questions";
+import loadQuestions from "../mocks/load-questions";
+import { useNavigate } from "react-router-dom";
+import { Paths } from "../constants";
+import { evaluateAnswers } from "../services/answer";
+import { Answer, Personality } from "../types";
+import PersonalityComponent from "../components/personality";
+
+const Quiz = () => {
+  const { questions, setQuestions } = useQuestions();
+  const [personality, setPersonality] = useState<Personality | undefined>();
+  const navigate = useNavigate();
+
+  const isCompleted = !!personality;
+  const isLoaded = !!questions;
+
+  useEffect(() => {
+    if (!questions) {
+      loadQuestions().then(setQuestions);
+    }
+  }, [questions, setQuestions]);
+
+  const goHome = useCallback(() => {
+    navigate(Paths.Home);
+  }, []);
+
+  const resolvePersonality = useCallback(
+    (answers: Answer[]) => {
+      const personality = evaluateAnswers(answers);
+      setPersonality(personality);
+    },
+    [setPersonality]
+  );
+
+  return (
+    <Container>
+      <h1 className="mt-3">Personality questionnaire</h1>
+      <Row>
+        <Col className="justify-content-center">
+          {isCompleted && <PersonalityComponent personality={personality} />}
+          {isLoaded && !isCompleted && (
+            <Questionnaire
+              questions={questions}
+              onCancel={goHome}
+              onComplete={resolvePersonality}
+            />
+          )}
+          {!isLoaded && <Spinner animation="border" />}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Quiz;
